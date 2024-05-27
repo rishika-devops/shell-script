b8b5aff432b22f477ca41342806e2e04a8e885f3
sowji <jujjurisowjanya@gmail.com>
2024-05-27 16:34:43 +0530

    new

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1=ramesh
-person2=suresh
+person1=$1
+person2=$2
 echo "$person1: hello $person2, good morning"
 echo "$person2: hello $person1, good morning"
 echo "$person1: how r u $person2?"
