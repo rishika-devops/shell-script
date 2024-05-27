fac1cd06d9ec9ab1b7e40cc700d8827cb20df418
sowji <jujjurisowjanya@gmail.com>
2024-05-27 16:27:28 +0530

    new

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "ramesh: hello suresh, good morning"
-echo "suresh: hello ramesh, good morning"
-echo "ramesh: how r u suresh?"
-echo "suresh: i am fine. how r u?"
+$1=ramesh
+$2=suresh
+echo "$1: hello $2, good morning"
+echo "$2: hello $1, good morning"
+echo "$1: how r u $2?"
+echo "$2: i am fine. how r u $1?"
