bd855d0e638d235068efdc976520cbf12cec05ce
sowji <jujjurisowjanya@gmail.com>
2024-05-27 16:40:06 +0530

    new

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 username=$1
 password=$2
-echo "username is $username"
+echo "pls enter ur username:"
 read username
-echo "password is $password"
+echo "username is $username"
+echo "pls enter ur password:"
 read password
+echo "password is $password"
