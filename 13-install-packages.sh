0521a2a1b292d0f5e81d908130739a7ffa3ec9b8
sowji <jujjurisowjanya@gmail.com>
2024-05-28 20:51:56 +0530

    new

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -7,7 +7,7 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script started executing at $TIMESTAMP"
 echo "arguments passed: $@"
-validate() {
+validate(){
     if [ $1 -ne 0 ]
     then
     echo -e "$R $2 is failed $N"
