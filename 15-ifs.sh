dd0b328e14822203dc6b7d6bdcb371495cd5e5bb
sowji <jujjurisowjanya@gmail.com>
2024-05-30 20:23:09 +0530

    new

--- 15-ifs.sh
+++ 15-ifs.sh
@@ -4,7 +4,7 @@ if [ ! -f $FILE ]
 then
 echo "$FILE doesnot exists"
 fi
-while IFS=":" read -r username password user_id
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
 do
 echo "username: $username"
 echo "password: $password"
