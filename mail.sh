3d2e66b17ad8613c39c881500e14c28f1b6b166d
sowji <jujjurisowjanya@gmail.com>
2024-05-30 21:57:52 +0530

    new

--- mail.sh
+++ mail.sh
@@ -6,4 +6,4 @@ ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 TO_ADDRESS=$4
 SUBJECT=$5
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
-echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\n content_Type: text/html")" "$TO_ADDRESS"
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent_Type: text/html")" "$TO_ADDRESS"
