6fa763eb511425831c234d5a9cf5780689cf63b8
sowji <jujjurisowjanya@gmail.com>
2024-05-30 21:23:18 +0530

    new

---
+++ 16-disk-usage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DISK_USAGE=$(df -hT | grep -i xfs)
+DISK_THRESHOLD=1
+message=""
+while ifs=read line
+do
+USAGE=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+partition=$(echo $line | awk '{print $1F}')
+if [ $USAGE -ge $DISK_THRESHOLD ]
+then
+message+= "high disk usage on $partition:$usage <br>"
+fi
+done <<< $DISK_USAGE
+echo "message: $message"
+sh mail.sh "devops team" "high disk usage" "$message" "jujjurisowjanya@gmail.com" "alert high disk usage"
+
+
+
---
+++ mail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+to_team=$1
+Alert_type=$2
+BODY=$3
+ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*>^[]/\\&/g');
+TO_ADDRESS=$4
+SUBJECT=$5
+FINAL_BODY=$(sed -e "s/to_team/$to_team/g" -e "s/Alert_type/$Alert_type/g" -e "s/BODY/$BODY/g" template.html)
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT" \n content_type: text/html")" "$TO_ADDRESS"
